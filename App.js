a9d37fb370cd0d9bcf66b237cde76f11edc682e7
Masashige-T <m.t0719@outlook.jp>
2020-08-07 17:58:08 +0900

    add sliceを結合しAppからstoreへ接続

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import { StatusBar } from "expo-status-bar"
 import React from "react"
 import { StyleSheet, Text, View } from "react-native"
+import { Provider } from "react-redux"
 
+import { store } from "./store"
 import { ButtonComp } from "./components/Button"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on my app!</Text>
-      <ButtonComp />
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on my app!</Text>
+        <ButtonComp />
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   )
 }
 
